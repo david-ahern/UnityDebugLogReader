4da76adea693154487f36012b853b42349432dd7
David Ahern <david-ahern@hotmail.co.uk>
2014-05-14 22:39:15 +0100

    Setup

---
+++ UnityDebugLogReader/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace UnityDebugLogReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.OpenStreamButton = new System.Windows.Forms.Button();
+            this.CloseStreamButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.SystemColors.Control;
+            this.Output.Location = new System.Drawing.Point(12, 12);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(691, 528);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // OpenStreamButton
+            // 
+            this.OpenStreamButton.Location = new System.Drawing.Point(505, 558);
+            this.OpenStreamButton.Name = "OpenStreamButton";
+            this.OpenStreamButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenStreamButton.TabIndex = 1;
+            this.OpenStreamButton.Text = "OpenStream";
+            this.OpenStreamButton.UseVisualStyleBackColor = true;
+            this.OpenStreamButton.Click += new System.EventHandler(this.OpenStreamButton_Click);
+            // 
+            // CloseStreamButton
+            // 
+            this.CloseStreamButton.Location = new System.Drawing.Point(603, 558);
+            this.CloseStreamButton.Name = "CloseStreamButton";
+            this.CloseStreamButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseStreamButton.TabIndex = 2;
+            this.CloseStreamButton.Text = "CloseStream";
+            this.CloseStreamButton.UseVisualStyleBackColor = true;
+            this.CloseStreamButton.Click += new System.EventHandler(this.CloseStreamButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(715, 593);
+            this.Controls.Add(this.CloseStreamButton);
+            this.Controls.Add(this.OpenStreamButton);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button OpenStreamButton;
+        private System.Windows.Forms.Button CloseStreamButton;
+    }
+}
+
---
+++ UnityDebugLogReader/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace UnityDebugLogReader
+{
+    public partial class Form1 : Form
+    {
+        Thread CheckLogThread;
+
+        FileStream Log;
+
+        private List<string> OutputLines;
+        int NumLinesOutput = 0;
+
+        OpenFileDialog fDialog;
+        string dir;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OutputLines = new List<string>();
+
+            dir = "";
+
+            fDialog = new OpenFileDialog();
+            fDialog.Title = "Open";
+            fDialog.Filter = "TextFiles|*.txt";
+            fDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            fDialog.FileOk += FileDirectorySelected;
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Reset();
+        }
+
+        private void CloseStreamButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void OpenStreamButton_Click(object sender, EventArgs e)
+        {
+            fDialog.ShowDialog();
+        }
+
+        private void FileDirectorySelected(object sender, CancelEventArgs e)
+        {
+            Reset();
+            dir = fDialog.FileName;
+            MessageBox.Show(e.ToString());
+            try
+            {
+                Log = new FileStream(dir, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            }
+            catch (IOException ex) 
+            {
+                MessageBox.Show("Could not open file\n" + ex.Message);
+            }
+
+            if (Log != null)
+            {
+                CheckLogThread = new Thread(CheckDebugLogFile);
+                CheckLogThread.Start();
+            }
+        }
+
+        private void CheckDebugLogFile()
+        {
+            long prevSize = 0;
+            
+            while (true)
+            {
+                if (prevSize < Log.Length)
+                {
+                    byte[] buffer = new byte[Log.Length];
+                    Log.Read(buffer, 0, (int)Log.Length);
+
+                    string str = System.Text.Encoding.ASCII.GetString(buffer);
+
+                    string[] stringArray = str.Split('\n');
+
+                    for (int i = 0; i < stringArray.Length; ++i)
+                    {
+                        OutputLines.Add(stringArray[i]);
+                    }
+
+                    PrintOutputToWindow();
+                    prevSize = Log.Length;
+                }
+                else if (prevSize > Log.Length)
+                {
+                    Log.Position = 0;
+                    OutputLines.Clear();
+                    NumLinesOutput = 0;
+                    prevSize = 0;
+                }
+                Thread.Yield();
+            }
+        }
+
+        private void PrintOutputToWindow()
+        {
+            for (int i = NumLinesOutput; i < OutputLines.Count; i++)
+            {
+                Output.AppendText(OutputLines[i]);
+                NumLinesOutput++;
+            }
+        }
+
+        private void Reset()
+        {
+            OutputLines.Clear();
+            NumLinesOutput = 0;
+            if (Log != null)
+                Log.Close();
+
+            if (CheckLogThread != null && CheckLogThread.IsAlive)
+                CheckLogThread.Abort();
+        }
+
+
+    }
+}
